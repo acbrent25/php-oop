ab35186b3710774dedcd16cdbaf4866c5bbc868b
Adam Champagne <adamchampagne@gmail.com>
2018-07-22 17:29:52 -0400

    private functions, magic method, getter and setter

---
+++ 2_5.php
@@ -0,0 +1,47 @@
+<?php
+
+    class User {
+        private $name;
+        private $age;
+
+        public function __construct($name, $age){
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        // Getter
+        public function getName(){
+            return $this->name;
+        }
+
+        // Setter
+        public function setName($name){
+            $this->name = $name;
+        }
+
+        // __get MAGIC METHOD
+        public function __get($property){
+            if(property_exists($this, $property)){
+                return $this->$property;
+            }
+        }
+
+        // __set MAGIC METHOD
+        public function __set($property, $value){
+            if(property_exists($this, $property)){
+                $this->$property = $value;
+            } 
+            return $this;           
+        }
+
+
+
+    }
+
+    $user1 = new User('Adam', 40);
+
+    // echo $user1->setName('Jeff');
+
+    // echo $user1->getName();
+    $user1->__set('age', 56);
+    echo $user1->__get('age');
